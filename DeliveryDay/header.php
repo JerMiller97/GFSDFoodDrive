457f3a5e49be53c45e1ea19cb00a4048c4181d4c
Jeremy Miller <jeremy.richard.miller@gmail.com>
2020-12-12 19:49:39 -0800

    Add DeliveryDay functions

---
+++ DeliveryDay/header.php
@@ -0,0 +1,66 @@
+<header class="navbar navbar-expand navbar-dark flex-column flex-md-row bd-navbar">
+  <a class="navbar-brand mr-0 mr-md-2" href="/" >North Pole
+</a>
+  <div class="navbar-nav-scroll">
+    <ul class="navbar-nav bd-navbar-nav flex-row">
+<?php 
+
+session_start();
+if ($_SESSION["PermissionLevel"] == 9 or $_SESSION["PermissionLevel"] == 7) {
+	?>
+
+
+		<li class="nav-item">
+        <a class="nav-link"  href="/Family/">Families</a>
+</li>
+	<?php 
+}
+
+if ($_SESSION["PermissionLevel"] == 9)
+	
+	print "
+
+		<li class=\"nav-item\">
+		<a class=\"nav-link\" href=\"/Santa/\">Santa</a>
+		</li>
+		<li class=\"nav-item\">
+		<a class=\"nav-link active\" href=\"/DeliveryDay/\">Delivery Day</a>
+		</li>
+
+	";
+	
+	if ($_SESSION["PermissionLevel"] == 9 or $_SESSION["PermissionLevel"] == 8)
+	
+	print "
+
+
+      <li class=\"nav-item\">
+        <a class=\"nav-link \" href=\"/GiftWorld/\">Gift World</a>
+      </li>
+      <li class=\"nav-item\">
+        <a class=\"nav-link\" href=\"/FoodWorld/\">Food World</a>
+      </li>      
+
+	  	  <li class=\"nav-item\">
+        <a class=\"nav-link \" href=\"/LiveDashboards/\">Dashboards</a>
+      </li>
+
+	";
+	
+?>
+
+
+    </ul>
+  </div>
+
+  <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
+    
+Hello, <?php 
+echo " " .ucwords($_SESSION["user"]);?>
+
+
+
+  </ul>
+
+  <a class="btn btn-bd-download d-none d-lg-inline-block mb-3 mb-md-0 ml-md-3" href="/logout.php">Logout</a>
+</header>
